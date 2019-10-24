796bce66010dc87cd148a5c43fde35c488510519
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-10-24 01:15:12 +0100

    Added unit tests

---
+++ src/Helper.TDateTime.pas
@@ -0,0 +1,110 @@
+unit Helper.TDateTime;
+
+interface
+
+type
+  TDateTimeHelper = record helper for TDateTime
+  public
+    function AsYear: word;
+    function AsMonth: word;
+    function AsDay: word;
+    function DatePart: TDateTime;
+    function AsHour: word;
+    function AsMinute: word;
+    function AsSecond: word;
+    function TimePart: TDateTime;
+    function ToString(const Format: string = ''): string;
+    function AsFloat: double;
+    function DayOfWeek: word;
+    function DayOfWeekName: string;
+    function DayOfWeekShortName: string;
+    function DaysInMonth: word;
+    function IncMonth(incerment: word): TDateTime;
+  end;
+
+implementation
+
+uses
+  System.SysUtils,
+  System.DateUtils;
+
+function TDateTimeHelper.AsFloat: Double;
+begin
+  Result := Self;
+end;
+
+function TDateTimeHelper.AsDay: Word;
+begin
+  Result := System.DateUtils.DayOf(Self);
+end;
+
+function TDateTimeHelper.DatePart: TDateTime;
+begin
+  Result := Int(Self);
+end;
+
+function TDateTimeHelper.AsHour: word;
+begin
+  Result := HourOf(Self);
+end;
+
+function TDateTimeHelper.AsMinute: word;
+begin
+  Result := MinuteOf(Self);
+end;
+
+function TDateTimeHelper.AsSecond: word;
+begin
+  Result :=SecondOf(Self);
+end;
+
+function TDateTimeHelper.TimePart: TDateTime;
+begin
+  Result := Frac(Self);
+end;
+
+function TDateTimeHelper.DayOfWeek: Word;
+begin
+  Result := System.SysUtils.DayOfWeek(Self);
+end;
+
+function TDateTimeHelper.DayOfWeekName: string;
+begin
+  Result := System.SysUtils.FormatDateTime ('dddd',Self);
+end;
+
+function TDateTimeHelper.DayOfWeekShortName: string;
+begin
+  Result := System.SysUtils.FormatDateTime ('ddd',Self)
+end;
+
+function TDateTimeHelper.DaysInMonth: word;
+begin
+  Result := System.DateUtils.DaysInMonth(Self);
+end;
+
+function TDateTimeHelper.AsMonth: Word;
+begin
+  Result := System.DateUtils.MonthOf(Self);
+end;
+
+function TDateTimeHelper.AsYear: Word;
+begin
+  Result := System.DateUtils.YearOf(Self);
+end;
+
+function TDateTimeHelper.ToString(const Format: string = ''): string;
+begin
+  if Format <> '' then
+    Result := System.SysUtils.FormatDateTime(Format, Self)
+  else
+    Result := System.SysUtils.DateToStr(Self);
+end;
+
+function TDateTimeHelper.IncMonth(incerment: word): TDateTime;
+begin
+  Result := System.SysUtils.IncMonth(Self,incerment);
+end;
+
+end.
+
---
+++ tests/Test.Main.pas
@@ -0,0 +1,103 @@
+﻿unit Test.Main;
+
+interface
+
+uses
+  DUnitX.TestFramework,
+  System.Classes,
+  System.SysUtils,
+
+  Helper.TDateTime;
+
+{$M+}
+
+type
+
+  [TestFixture]
+  TDate2019_10_24_T_21_15_59 = class(TObject)
+  private
+    fDate: TDateTime;
+  public
+    [Setup]
+    procedure Setup;
+  published
+    procedure Test_AsYear;
+    procedure Test_AsDay;
+    procedure Test_AsMonth;
+    procedure Test_DatePart;
+    procedure Test_AsHour;
+    procedure Test_AsMinute;
+    procedure Test_AsSeconds;
+  end;
+
+implementation
+
+// -----------------------------------------------------------------------
+// Setup and TearDown section
+// -----------------------------------------------------------------------
+
+procedure TDate2019_10_24_T_21_15_59.Setup;
+begin
+  fDate := EncodeDate(2019,10,24) + EncodeTime(21,15,59,0);
+end;
+
+// -----------------------------------------------------------------------
+// Tests section 1
+// -----------------------------------------------------------------------
+{$REGION 'Test section 1'}
+
+procedure TDate2019_10_24_T_21_15_59.Test_AsYear;
+begin
+  Assert.AreEqual(2019, fDate.AsYear);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_AsMonth;
+begin
+  Assert.AreEqual(10, fDate.AsMonth);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_AsDay;
+begin
+  Assert.AreEqual(24, fDate.AsDay);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_DatePart;
+begin
+  Assert.AreEqual(double(43762), fDate.DatePart);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_AsHour;
+begin
+  Assert.AreEqual(21, fDate.AsHour);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_AsMinute;
+begin
+  Assert.AreEqual(15, fDate.AsMinute);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_AsSeconds;
+begin
+  Assert.AreEqual(59, fDate.AsSecond);
+end;
+
+  (*
+    Minute: word;
+    Second: word;
+    TimePart: TDateTime;
+    ToString(const Format: string = ''): string;
+    AsFloat: double;
+    DayOfWeek: word;
+    DayOfWeekName: string;
+    DayOfWeekShortName: string;
+    DaysInMonth: word;
+    IncMonth(incerment: word): TDateTime;
+  *)
+
+{$ENDREGION}
+
+initialization
+
+TDUnitX.RegisterTestFixture(TDate2019_10_24_T_21_15_59);
+
+end.
