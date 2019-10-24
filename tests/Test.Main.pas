973d98b4b3c0c3761a4bdc9ce7f5dd5ec69ad56f
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-10-24 02:00:39 +0100

    Implemented unit tests

--- tests/Test.Main.pas
+++ tests/Test.Main.pas
@@ -17,9 +17,12 @@ type
   TDate2019_10_24_T_21_15_59 = class(TObject)
   private
     fDate: TDateTime;
+    fExpected: TDateTime;
   public
     [Setup]
     procedure Setup;
+    [TearDown]
+    procedure TearDown;
   published
     procedure Test_AsYear;
     procedure Test_AsDay;
@@ -28,6 +31,15 @@ type
     procedure Test_AsHour;
     procedure Test_AsMinute;
     procedure Test_AsSeconds;
+    procedure Test_TimePart;
+    procedure Test_ToString_Empty;
+    procedure Test_ToString_yymmdd;
+    procedure Test_AsFloat;
+    procedure Test_DayOfWeek;
+    procedure Test_DayOfWeekName;
+    procedure Test_DayOfWeekShortName;
+    procedure Test_DaysInMonth;
+    procedure Test_IncMonth_5;
   end;
 
 implementation
@@ -39,6 +51,12 @@ implementation
 procedure TDate2019_10_24_T_21_15_59.Setup;
 begin
   fDate := EncodeDate(2019,10,24) + EncodeTime(21,15,59,0);
+  FormatSettings.DateSeparator := '-';
+  FormatSettings.ShortDateFormat := 'yyyy/mm/dd';
+end;
+
+procedure TDate2019_10_24_T_21_15_59.TearDown;
+begin
 end;
 
 // -----------------------------------------------------------------------
@@ -81,17 +99,58 @@ begin
   Assert.AreEqual(59, fDate.AsSecond);
 end;
 
+procedure TDate2019_10_24_T_21_15_59.Test_TimePart;
+begin
+  fExpected := 21*(1/24) + 15*(1/24/60) + 59*(1/24/60/60);
+  Assert.AreEqual(fExpected, fDate.TimePart, 0.00000001);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_ToString_Empty;
+begin
+  Assert.AreEqual('2019-10-24', fDate.ToString());
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_ToString_yymmdd;
+begin
+
+  Assert.AreEqual('24 '+FormatSettings.ShortMonthNames[10]+' 2019',
+    fDate.ToString('dd mmm yyyy'));
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_AsFloat;
+begin
+  Assert.AreEqual(double(43762.886099537), fDate.AsFloat, 0.00000001);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_DayOfWeek;
+begin
+  Assert.AreEqual(5, fDate.DayOfWeek);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_DayOfWeekName;
+begin
+  Assert.AreEqual(FormatSettings.LongDayNames[5], fDate.DayOfWeekName);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_DayOfWeekShortName;
+begin
+  Assert.AreEqual(FormatSettings.ShortDayNames[5], fDate.DayOfWeekShortName);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_DaysInMonth;
+begin
+  Assert.AreEqual(31, fDate.DaysInMonth);
+end;
+
+procedure TDate2019_10_24_T_21_15_59.Test_IncMonth_5;
+begin
+  Assert.AreEqual(Int(EncodeDate(2020,3,24)), Int(fDate.IncMonth(5)) );
+end;
+
   (*
-    Minute: word;
-    Second: word;
-    TimePart: TDateTime;
-    ToString(const Format: string = ''): string;
-    AsFloat: double;
-    DayOfWeek: word;
-    DayOfWeekName: string;
-    DayOfWeekShortName: string;
-    DaysInMonth: word;
-    IncMonth(incerment: word): TDateTime;
+    : string;
+    : word;
+    (incerment: word): TDateTime;
   *)
 
 {$ENDREGION}
