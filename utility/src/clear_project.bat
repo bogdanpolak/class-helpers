fa17cf6c22ffd156421e9845c1c7ac6c24ac382b
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-11-28 22:41:53 +0000

    Added command-line tool version_bumper

---
+++ utility/src/clear_project.bat
@@ -0,0 +1,13 @@
+rmdir /Q /S __history
+rmdir /Q /S Win32
+rmdir /Q /S .svn
+rmdir /Q /S __recovery
+
+del *.identcache
+del *.dproj.local
+del desktop.ini
+del *.stat
+
+del *.dcu
+del *.exe
+del *.dll
