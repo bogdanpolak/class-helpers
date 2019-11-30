aae4aa32beabe41fe34ce76c12a2784f39c2379f
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-11-30 00:54:12 +0000

    Added TStringGrid helper

---
+++ src/Helper.TStringGrid.pas
@@ -0,0 +1,40 @@
+﻿unit Helper.TStringGrid;
+
+interface
+
+uses
+  System.JSON,
+  System.SysUtils,
+  System.DateUtils,
+  Vcl.Grids;
+
+type
+  TStringGridHelper = class helper for TStringGrid
+  const
+    // ♥ ------------------------------------------------------------------ ♥
+    ReleaseDate = '2019.11.30';
+    ReleaseVersion = '1.1';
+    // ♥ ------------------------------------------------------------------ ♥
+  public
+    procedure ColsWidth(aWidths: TArray<Integer>);
+  end;
+
+implementation
+
+resourcestring
+  StrErrorTooManyColumns = 'Too many data in width table (array size = %d, b' +
+  'ut grid has %d collumns';
+
+procedure TStringGridHelper.ColsWidth(aWidths: TArray<Integer>);
+var
+  i: Integer;
+begin
+  if ColCount < Length(aWidths) then
+    raise ERangeError.Create
+      (Format(StrErrorTooManyColumns,
+      [Length(aWidths), ColCount]));
+  for i := 0 to High(aWidths) do
+    ColWidths[i] := aWidths[i];
+end;
+
+end.
