a28cb085867c077fb1694dd54105d1f5a5180748
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-11-30 01:16:39 +0000

    Formated code

--- src/Helper.TStringGrid.pas
+++ src/Helper.TStringGrid.pas
@@ -22,16 +22,15 @@ type
 implementation
 
 resourcestring
-  StrErrorTooManyColumns = 'Too many data in width table (array size = %d, b' +
-  'ut grid has %d collumns';
+  StrErrorTooManyColumns = 'Too many data in width table ' +
+    '(array size = %d, but grid has %d collumns)';
 
 procedure TStringGridHelper.ColsWidth(aWidths: TArray<Integer>);
 var
   i: Integer;
 begin
   if ColCount < Length(aWidths) then
-    raise ERangeError.Create
-      (Format(StrErrorTooManyColumns,
+    raise ERangeError.Create(Format(StrErrorTooManyColumns,
       [Length(aWidths), ColCount]));
   for i := 0 to High(aWidths) do
     ColWidths[i] := aWidths[i];
