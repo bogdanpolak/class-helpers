93cee0e7d924269d80df30db45ba48fa82e7e8d0
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-12-07 22:41:54 +0000

    Added test ForEachRowVisitedDates

--- tests/Test.Helper.TDataSet.pas
+++ tests/Test.Helper.TDataSet.pas
@@ -28,6 +28,7 @@ type
     procedure TearDown;
   published
     procedure GetMaxIntegerValue_546;
+    procedure ForEachRowVisitedDates;
   end;
 
 implementation
@@ -80,6 +81,31 @@ begin
   aMaxValue := fDataset.GetMaxIntegerValue('rank');
   // Assert
   Assert.AreEqual(6, aMaxValue);
+end;
+
+procedure TestTDataSetHelper.ForEachRowVisitedDates;
+var
+  visitedField: TDateTimeField;
+  s: string;
+begin
+  // Arrange
+  BuildDataSet1;
+  // .   GivenDataSetWithFourVisitedCities
+  fDataset.AppendRecord([1, 'Edynburgh', 5, EncodeDate(2018, 05, 28)]);
+  fDataset.AppendRecord([2, 'Glassgow', 4, EncodeDate(2015, 09, 13)]);
+  fDataset.AppendRecord([3, 'Cracow', 6, EncodeDate(2019, 01, 01)]);
+  fDataset.AppendRecord([4, 'Prague', 4, EncodeDate(2013, 06, 21)]);
+  visitedField := fDataset.FieldByName('visited') as TDateTimeField;
+  fDataset.First;
+  s := '';
+  // Act
+  fDataset.ForEachRow(
+    procedure
+    begin
+      s := s + FormatDateTime('yyyy-mm', visitedField.Value)+' '
+    end);
+  // Assert
+  Assert.AreEqual ('2018-05 2015-09 2019-01 2013-06 ',s);
 end;
 
 initialization
