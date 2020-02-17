50e40dbb039b3c121048507a884768142603399b
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-02-17 23:40:40 +0000

    Added stub interface

--- src/Helper.TBytes.pas
+++ src/Helper.TBytes.pas
@@ -3,6 +3,7 @@ unit Helper.TBytes;
 interface
 
 uses
+  System.Classes,
   System.SysUtils;
 
 type
@@ -10,10 +11,16 @@ type
   private
   public
     // ---------------------
-    // Base / utility methods:
+    // Size:
     function GetSize: Integer;
     procedure SetSize(aSize: Integer);
     property Size: Integer read GetSize write SetSize;
+    // ---------------------
+    // InitialiseFrom / Load / Save
+    procedure LoadFromStream(const aStream: TStream);
+    procedure LoadFromFile(const aFileName: string);
+    procedure SaveToStream(const aStream: TStream);
+    procedure SaveToFile(const aFileName: string);
     procedure InitialiseFromBase64String(const aBase64Str: String);
   end;
 
@@ -21,6 +28,10 @@ implementation
 
 uses
   System.NetEncoding;
+
+// -----------------------------------------------------------------------
+// Size
+// -----------------------------------------------------------------------
 
 function TBytesHelper.GetSize: Integer;
 begin
@@ -32,9 +43,33 @@ begin
   SetLength(Self, aSize);
 end;
 
+// -----------------------------------------------------------------------
+// InitialiseFrom / Load / Save
+// -----------------------------------------------------------------------
+
 procedure TBytesHelper.InitialiseFromBase64String(const aBase64Str: String);
 begin
   Self := TNetEncoding.Base64.DecodeStringToBytes(aBase64Str);
+end;
+
+procedure TBytesHelper.LoadFromFile(const aFileName: string);
+begin
+
+end;
+
+procedure TBytesHelper.LoadFromStream(const aStream: TStream);
+begin
+
+end;
+
+procedure TBytesHelper.SaveToFile(const aFileName: string);
+begin
+
+end;
+
+procedure TBytesHelper.SaveToStream(const aStream: TStream);
+begin
+
 end;
 
 end.
