610d7b14f9fa3d1f569aa6b3e717964d8c0f0338
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-02-20 00:12:02 +0000

    Added xmldoc for TStreamHelper

--- src/Helper.TStream.pas
+++ src/Helper.TStream.pas
@@ -14,12 +14,41 @@ type
     Version = '1.6';
   private
   public
+    /// <summary>
+    ///   Saves the entire contents of the stream to a file (method can be
+    ///   overwritten in inherited classes)
+    /// </summary>
     procedure SaveToFile(aFileName: string);
+    /// <summary>
+    ///   Saves whole contet of the stream into a temporary file in the
+    ///   temporary operating system folder (for Windows 10 it is:
+    ///   `C:\Users\{profile}\AppData\Local\Temp\`. This method returns
+    ///   a name of newlly created file
+    ///   Saves the entire stream content to a temporary file in the temporary
+    ///   operating system folder (for Windows 10:
+    ///   `C:\Users\{profile}\AppData\Local\Temp\`). This method returns the
+    ///   name of the newly created file
+    /// </summary>
     function SaveToTempFile: string;
+    /// <summary>
+    ///   Reads whole content of the stream interprets this as UTF8 unicode
+    ///   text and return as String. Accepts the optional "aEncoding" parameter,
+    ///   which allows stream bytes to be interpreted as differently encoded
+    ///   text, eg: ANSI text, UTF16 text or other.
+    /// </summary>
     function AsString: string; overload;
     function AsString(aEncoding: TEncoding): string; overload;
+    /// <summary>
+    ///   Writes a string to the stream using the UTF8 encodeing (by default)
+    ///   or any other provided encoding.
+    /// </summary>
     procedure WriteString(const aText: string); overload;
     procedure WriteString(const aText: string; aEncoding: TEncoding); overload;
+    /// <summary>
+    ///   Writes a string finished with thne system End of Line code to the
+    ///   stream using the UTF8 encodeing (by default) or any other provided
+    ///   encoding.
+    /// </summary>
     procedure WriteLine(const aText: string); overload;
     procedure WriteLine(const aText: string; aEncoding: TEncoding); overload;
   end;
