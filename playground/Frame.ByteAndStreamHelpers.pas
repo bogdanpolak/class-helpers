f8ff381a36bc1b2b1320db689d39c61e9476572e
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-02-22 21:08:56 +0000

    Implemented demo

--- playground/Frame.ByteAndStreamHelpers.pas
+++ playground/Frame.ByteAndStreamHelpers.pas
@@ -293,7 +293,42 @@ end;
 // ------------------------------------------------------------------------
 
 procedure TBytesStreamHelpersFrame.btnSaveAsTempFileClick(Sender: TObject);
+var
+  aBytes: TBytes;
+  ms: TMemoryStream;
+  png: TPngImage;
+  aFileName: string;
+  aTempPngFileName: string;
 begin
+  Memo1.Lines.Add('--------------------------------');
+  aBytes.InitialiseFromBase64String
+    ('iVBORw0KGgoAAAANSUhEUgAAAFsAAAAaCAMAAADv7NBiAAAAe1BMVEXw8PD/' +
+    '///Z2dnwz4c3NofP8PAAAAAoKSmHz/Dw8Ks2AgA1h8+r8PDPz4cANodgEmBd' +
+    'qfIABDarYADwq2DPhzeHNgAAYKvw8M+rz4dgCzZgBwA2DmAADGA5ZLPP8Ktg' +
+    'YKtlTzk2BjZks9ir8Ks4ZWU4N2VnZkWPZQBgNgB6DF3BAAABJUlEQVRIx+2T' +
+    '25LCIAyGU7BZ2m67ldq6B9fVPen7P6EhNGMd8EoZxxlzw58QPgIB2EA6ezrK' +
+    'B5tMKZWOnWUjW13fjmy4tj3Yt2XrErECWKAb9CwHiU5dUaHJbJT9ZqD4gkWT' +
+    'Q/FeSVLB0YvZL8+exSm1sDm67Kl4OtDcsNIlKf23x+8eW/DebIfY8myMbXHg' +
+    'jZn4yqNEpZ5ucIr2c7tvTTc3XbP2XtlC1+TxunkpDgHbRZnIN8+rncAVZZAm' +
+    'qniUdJbNzJ/TO5F7ccu2hlcLQHTtvZD9P2F/cobvZStJ2kf1h8NTo52i6Ql7' +
+    'Ld6YF627d73iN+gbhCvKW45Ri5VF/CUCKb6dWs4gHvNt2MvwIV3+dzYxNlgq' +
+    '9X7+fAq2SmHCzhJYWvYBmzQSEHa+IRoAAAAASUVORK5CYII=');
+  ms := TMemoryStream.Create;
+  aBytes.SaveToStream(ms);
+  ms.Position := 0;
+  // -----
+  aFileName := ms.SaveToTempFile;
+  Memo1.Lines.Add('1. Saved PNG stream to temp file: ' + aFileName);
+  aTempPngFileName := ChangeFileExt(aFileName, '.png');
+  Memo1.Lines.Add('2. Renamed temp file to: ' + aTempPngFileName);
+  RenameFile(aFileName, aTempPngFileName);
+  ms.Free;
+  // -----
+  png := TPngImage.Create;
+  Memo1.Lines.Add('3. Loaded PNG image form file');
+  png.LoadFromFile(aTempPngFileName);
+  Image1.Picture.Graphic := png;
+  png.Free;
 end;
 
 procedure TBytesStreamHelpersFrame.btnWriteTextToStreamClick(Sender: TObject);
