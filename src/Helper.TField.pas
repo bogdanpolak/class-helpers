82d1d8b9e8904f0d9451ca2673b7a15c2041fe45
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-02-23 00:48:24 +0000

    [green] Done SetBlobFromBase64String

--- src/Helper.TField.pas
+++ src/Helper.TField.pas
@@ -13,8 +13,22 @@ type
     Version = '1.6';
   private
   public
+    function SetBlobFromBase64String(const aBase64Str: String): TBlobField;
   end;
 
 implementation
+
+uses
+  System.NetEncoding;
+
+function TFieldHelper.SetBlobFromBase64String(const aBase64Str: String)
+  : TBlobField;
+begin
+  if not(Self is TBlobField) then
+    raise EDatabaseError.Create('Invalid field type.' +
+      ' Method LoadFromBase64String is supported only for blob fields');
+  Result := (Self as TBlobField);
+  Result.Value := TNetEncoding.Base64.DecodeStringToBytes(aBase64Str);
+end;
 
 end.
