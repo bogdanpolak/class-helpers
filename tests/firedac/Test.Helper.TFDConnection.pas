72d46b2b3acfa07f7dddf4d854cb43f904986013
Bogdan Polak <bogdanpol@gmail.com>
2020-10-20 22:28:14 +0200

    Added test project

---
+++ tests/firedac/Test.Helper.TFDConnection.pas
@@ -0,0 +1,44 @@
+﻿unit Test.Helper.TFDConnection;
+
+interface
+
+uses
+  DUnitX.TestFramework,
+  System.Classes,
+  System.SysUtils,
+  System.IOUtils,
+
+  Helper.TFDConnection;
+
+{$M+}
+
+type
+
+  [TestFixture]
+  TestTFDConnectionHelper = class(TObject)
+  private
+  public
+    [Setup]
+    procedure Setup;
+  published
+    procedure T01;
+  end;
+
+implementation
+
+
+procedure TestTFDConnectionHelper.Setup;
+begin
+
+end;
+
+procedure TestTFDConnectionHelper.T01;
+begin
+  Assert.Fail('WIP');
+end;
+
+initialization
+
+TDUnitX.RegisterTestFixture(TestTFDConnectionHelper);
+
+end.
---
+++ tests/firedac/Test.Helper.TFDCustomManager.pas
@@ -0,0 +1,44 @@
+﻿unit Test.Helper.TFDCustomManager;
+
+interface
+
+uses
+  DUnitX.TestFramework,
+  System.Classes,
+  System.SysUtils,
+  System.IOUtils,
+
+  Helper.TFDCustomManager;
+
+{$M+}
+
+type
+
+  [TestFixture]
+  TestTFDCustomManagerHelper = class(TObject)
+  private
+  public
+    [Setup]
+    procedure Setup;
+  published
+    procedure T01;
+  end;
+
+implementation
+
+
+procedure TestTFDCustomManagerHelper.Setup;
+begin
+
+end;
+
+procedure TestTFDCustomManagerHelper.T01;
+begin
+  Assert.Fail('WIP');
+end;
+
+initialization
+
+TDUnitX.RegisterTestFixture(TestTFDCustomManagerHelper);
+
+end.
