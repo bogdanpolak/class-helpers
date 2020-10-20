a4ea4e833d814400fc517690273369a03a9803c6
Bogdan Polak <bogdanpol@gmail.com>
2020-10-21 00:00:20 +0200

    Added tests for WithConnectionDef

--- tests/firedac/Test.Helper.TFDConnection.pas
+++ tests/firedac/Test.Helper.TFDConnection.pas
@@ -17,24 +17,56 @@ type
   [TestFixture]
   TestTFDConnectionHelper = class(TObject)
   private
+    fOwner: TComponent;
   public
     [Setup]
     procedure Setup;
+    [TearDown]
+    procedure TearDown;
   published
-    procedure T01;
+    procedure CheckExistingConnectionDefinitions;
+    procedure WithConnectionDef_SQLite_Demo;
   end;
 
 implementation
 
+uses
+  FireDAC.Stan.Intf,
+  FireDAC.Phys.Intf,
+  FireDAC.Stan.Def,
+  FireDAC.Phys,
+  FireDAC.Phys.SQLite,
+  FireDAC.Phys.SQLiteDef,
+  FireDAC.Comp.Client;
 
 procedure TestTFDConnectionHelper.Setup;
 begin
-
+  fOwner := TComponent.Create(nil);
 end;
 
-procedure TestTFDConnectionHelper.T01;
+procedure TestTFDConnectionHelper.TearDown;
 begin
-  Assert.Fail('WIP');
+  fOwner.Free;
+end;
+
+procedure TestTFDConnectionHelper.CheckExistingConnectionDefinitions;
+var
+  Def: IFDStanDefinition;
+begin
+  Def := FDManager.ConnectionDefs.FindDefinition('SQLite_Demo');
+  Assert.IsTrue(Def <> nil,
+    'Definition "SQLite_Demo" is required to run tests');
+end;
+
+procedure TestTFDConnectionHelper.WithConnectionDef_SQLite_Demo;
+var
+  connection: TFDConnection;
+begin
+  connection := TFDConnection.Create(fOwner).WithConnectionDef('SQLite_Demo');
+  Assert.AreEqual('SQLite_Demo', connection.ConnectionDefName);
+  connection.Open();
+  Assert.IsTrue(connection.Connected,
+    'Not able to connect to SQLite database using "SQLite_Demo" definition');
 end;
 
 initialization
